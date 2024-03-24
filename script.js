703678184529cfc987ff510f899c03e4892a1d97
SanishPoudel <mr.sanish.poudel@gmail.com>
2024-03-24 12:05:03 +1100

    Removed unnecessary functions and fixed operator bugs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let clear = document.querySelector(".clear");
 let backspace = document.querySelector(".delete");
 let disp1 = document.querySelector(".disp1");
 let disp2 = document.querySelector(".disp2");
+let symbol = document.querySelector(".symbol");
 
 // Creating variables to perform calculations
 let initial = "", final = "", sign = "", result = "";
@@ -12,14 +13,10 @@ let initial = "", final = "", sign = "", result = "";
 
 // adding event listener for clearing the display
 clear.addEventListener("click", function clear() {
-    while (disp2.firstChild) {
-        disp2.removeChild(disp2.firstChild);
-    }
-    while (disp1.firstChild) {
-        disp1.removeChild(disp1.firstChild);
-    }
-    disp2.textContent = "0";
-    enable_buttons();
+    disp1.textContent = "";
+    disp2.textContent = "";
+    symbol.textContent = "";
+    sign = ""
 });
 
 
@@ -36,17 +33,27 @@ backspace.addEventListener("click", () => {
 // adding event listener for entering the numbers
 nums.forEach(num => {
     num.addEventListener("click", () => {
-        disp2.textContent += num.textContent;
+        if (sign != "=") {
+            disp2.textContent += num.textContent;
+        }
     })
 });
 
 
 operators.forEach(operator => {
     operator.addEventListener("click", () => { 
+        
+        // 2.> incase the user enters multiple operators, it'll change the operators.
+        if ((disp1.textContent) && (!disp2.textContent) && (symbol.textContent)) {
+            sign = operator.textContent; 
+            symbol.textContent = sign;
+            return
+        }
 
-        // if there hasn't been previous calculations
+        // 3.> if nothing goes wrong you should reach here. the calculation is done here.
         if (disp1.textContent) {
 
+            // initial is stored in step 1 below. sign is stored in either 1 or 2 depending on the user.
             final = disp2.textContent;
             result = (calculate(initial, final, sign));
 
@@ -56,24 +63,28 @@ operators.forEach(operator => {
             sign = operator.textContent;
             disp2.textContent = ""; 
             
+            // displaying output based on the operators.
             if (sign === "=") {
                 disp1.textContent = result;
-                disable_buttons();
+                symbol.textContent = sign;
             } else {
-                disp1.textContent = result + sign; 
+                disp1.textContent = result;
+                symbol.textContent = sign; 
             }
         }
 
         sign = operator.textContent;
 
+        // 1> this step happens first if you're using the calc for the first time. if you've already passed this step you reach the second step
         if (disp2.textContent) {
             initial = disp2.textContent;
 
+            // displaying result based on the 
             if (sign === "=") {
-                disp1.textContent = disp2.textContent;
-                disable_buttons();
+                return
             } else {
-                disp1.textContent = disp2.textContent + sign;  
+                disp1.textContent = disp2.textContent;
+                symbol.textContent = sign;  
             } 
             
             disp2.textContent = "";
@@ -92,22 +103,4 @@ function calculate(initial, final, sign) {
     } else if (sign === "÷") {
         return Number(initial) / Number(final);
     } 
-}
-
-function disable_buttons() {
-    operators.forEach(operator => {
-        operator.disabled = true;
-    })
-    nums.forEach(num => {
-        num.disabled = true;
-    })
-}
-
-function enable_buttons() {
-    operators.forEach(operator => {
-        operator.disabled = false;
-    })
-    nums.forEach(num => {
-        num.disabled = false;
-    })
 }
